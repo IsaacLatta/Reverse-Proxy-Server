46195fc94b02b8dd196f22e78800b8b556549c4a
Isaac <isaaclatta73@gmail.com>
2024-09-10 17:36:42 -0700

    Added cmd line parsing for https

--- Proxy/main.cpp
+++ Proxy/main.cpp
@@ -1,26 +1,40 @@
 #include "RevProxy.h"
 #include <iostream>
 
-// ./rev-proxy <host_port> <peer_IP> <peer_port> <path/to/cert> <path/to/key>
+
+void display_exit()
+{
+    std::string help_msg = "USAGE: ./rev-proxy <host_port> <peer_IP> <peer_port> -<option> <arguments>...\n"
+                            "OPTIONS: -d, daemon the proxy, -https, use https\n";
+    std::cout << help_msg;
+    exit(0);
+}
+
 int main(int argc, char** argv)
 {
-    if(argc != 4 || argv[1] == "-h")
+    if( argc == 1 || argv[1] == "-h")
     {
-        std::cout << "USAGE: ./rev-proxy <host_port> <peer_IP> <peer_port>\n";
-        return 0;
+        display_exit();
     }
+
+    std::unique_ptr<RevProxy> proxy;
+    int host_port = atoi(argv[1]);
+    int peer_port = atoi(argv[3]);
+    std::string peer_IP(argv[2]), cert_path, key_path;
+    if(peer_IP == "localhost") peer_IP = "127.0.0.1";
+
+    if(argc >= 6 && !strcmp(argv[4],"-https"))
+    {
+        cert_path = argv[5];
+        key_path = argv[6];
+        proxy = std::make_unique<RevProxy>(host_port, peer_IP, peer_port, cert_path, key_path, true);
+    }
+    else
+         proxy = std::make_unique<RevProxy>(host_port, peer_IP, peer_port);        
 
     try
     {
-        int host_port = atoi(argv[1]);
-        int peer_port = atoi(argv[3]);
-        std::string peer_IP(argv[2]);
-        
-        if(peer_IP == "localhost") peer_IP = "127.0.0.1";
-        
-        std::cout << peer_IP << "\n";
-        RevProxy proxy(host_port, peer_IP, peer_port);
-        proxy.run();
+        proxy->run();
     }
     catch(const std::exception& e)
     {
